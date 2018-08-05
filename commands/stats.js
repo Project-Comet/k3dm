89d3b8d6a52429653ae5d66dbb9bfc6ad85d5eff
Robert <nggroberto@gmail.com>
2018-08-05 19:26:02 +0200

    alternate method

--- commands/stats.js
+++ commands/stats.js
@@ -2,13 +2,15 @@ const config = require('../config.json');
 const packages = require('../package.json');
 const memUsage = process.memoryUsage()
 const bot = require('../bot.js')
+const { version } = require("discord.js");
+
 exports.run = async function(client, message, args) {
   let totalSeconds = (client.uptime / 1000);
   let hours = Math.floor(totalSeconds / 3600);
   totalSeconds %= 3600;
   let minutes = Math.floor(totalSeconds / 60);
   let seconds = totalSeconds % 60;
-  message.channel.send(`= Bot Statistics =\n❯ Memory        :: ${(memUsage.rss / 1048576).toFixed()}mb\n❯ Uptime        :: ${hours} hrs, ${minutes} mins & ${seconds.toFixed()} secs\n❯ Total Users   :: ${client.users.size}\n❯ Total Servers :: ${client.guilds.size}\n❯ Discord.js    :: ${packages.dependencies.discordjs}\n❯ Node          :: ${process.version} `, {code:'asciidoc'});
+  message.channel.send(`= Bot Statistics =\n❯ Memory        :: ${(memUsage.rss / 1048576).toFixed()}mb\n❯ Uptime        :: ${hours} hrs, ${minutes} mins & ${seconds.toFixed()} secs\n❯ Total Users   :: ${client.users.size}\n❯ Total Servers :: ${client.guilds.size}\n❯ Discord.js    :: ${version}\n❯ Node          :: ${process.version} `, {code:'asciidoc'});
 };
 
 
